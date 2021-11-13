a801991d3feb05aa99d1d58dfb293d3bef14338f
kmkmtrtr <arctic-wind@hotmail.co.jp>
2021-11-13 21:39:25 +0900

    implements search

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -2,7 +2,6 @@ import 'package:derasika2/data/model/score_data.dart';
 import 'package:derasika2/ui/component/loading_container.dart';
 import 'package:derasika2/ui/pages/home/home_view_model.dart';
 import 'package:derasika2/ui/pages/home/score_tile.dart';
-import 'package:draggable_scrollbar/draggable_scrollbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,7 +9,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 class HomePage extends HookConsumerWidget {
   HomePage({Key? key}) : super(key: key);
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  final myScrollController = ScrollController();
+  final _scrollController = ScrollController();
+  final _textFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -26,7 +26,42 @@ class HomePage extends HookConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         key: _scaffoldKey,
-        title: const Text('スコア一覧'),
+        title: TextField(
+          controller: _textFieldController,
+          decoration: InputDecoration(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            suffixIcon: _textFieldController.text != ''
+                ? IconButton(
+                    onPressed: () {
+                      _scrollController.jumpTo(0);
+                      _textFieldController.clear();
+                      ref.watch(homeViewModelProvider).changeFilterQuery('');
+                    },
+                    icon: const Icon(Icons.clear),
+                    color: Colors.white,
+                  )
+                : null,
+            hintText: ' Search...',
+            hintStyle: const TextStyle(color: Colors.white),
+          ),
+          style: const TextStyle(color: Colors.white),
+          onChanged: (s) {
+            _scrollController.jumpTo(0);
+            ref.watch(homeViewModelProvider).changeFilterQuery(s);
+          },
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(icon: const Icon(Icons.download), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.sort), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.filter_alt), onPressed: () {}),
+        ],
         // actions: <Widget>[
         // CsvDownloadButton(model),
         // SortButton(model),
@@ -36,10 +71,12 @@ class HomePage extends HookConsumerWidget {
       body: Center(
         child: LoadingContainer(
           isLoaded: snapshot.connectionState == ConnectionState.done,
-          child: DraggableScrollbar.semicircle(
-            controller: myScrollController,
+          child: Scrollbar(
+            controller: _scrollController,
+            isAlwaysShown: true,
+            interactive: true,
             child: ListView.builder(
-              controller: myScrollController,
+              controller: _scrollController,
               scrollDirection: Axis.vertical,
               itemCount: scoreList.length,
               itemBuilder: (BuildContext context, int index) {
@@ -50,14 +87,14 @@ class HomePage extends HookConsumerWidget {
         ),
       ),
       // drawer: HomeDrawer(),
-      floatingActionButton: snapshot.connectionState == ConnectionState.done
-          ? FloatingActionButton(
-              onPressed: () async {
-                ref.watch(homeViewModelProvider).changeVersion();
-              },
-              child: const Icon(Icons.change_circle_outlined),
-            )
-          : null,
+      // floatingActionButton: snapshot.connectionState == ConnectionState.done
+      //     ? FloatingActionButton(
+      //         onPressed: () async {
+      //           ref.watch(homeViewModelProvider).changeVersion();
+      //         },
+      //         child: const Icon(Icons.change_circle_outlined),
+      //       )
+      //     : null,
     );
   }
 }
