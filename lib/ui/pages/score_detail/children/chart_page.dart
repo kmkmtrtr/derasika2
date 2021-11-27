f5832032fa198069c9faed6da3173039d42621b3
kmkmtrtr <arctic-wind@hotmail.co.jp>
2021-11-28 00:47:28 +0900

    implements chart page

--- lib/ui/pages/score_detail/children/chart_page.dart
+++ lib/ui/pages/score_detail/children/chart_page.dart
@@ -1,6 +1,7 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
+import 'package:collection/collection.dart';
 import '../score_view_model.dart';
 
 class ChartPage extends HookConsumerWidget {
@@ -11,150 +12,103 @@ class ChartPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final scoreViewModel = ref.watch(scoreViewModelProvider(chartId));
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Card(
-            margin: const EdgeInsets.all(10),
-            child: Container(
-              padding: const EdgeInsets.all(5),
-              width: double.infinity,
-              child: Column(
-                children: [
-                  const Text(
-                    'Score Detail',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        children: [
-                          const Text('Historical Score'),
-                          Text(
-                            scoreViewModel.historicalScoreRecord?.score
-                                    .toString() ??
-                                '',
-                            style: const TextStyle(fontSize: 36),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          const Text('Current Score'),
-                          Text(
-                            scoreViewModel.currentRecord?.score.toString() ??
-                                '',
-                            style: const TextStyle(fontSize: 36),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        children: [
-                          const Text('Historical misscount'),
-                          Text(
-                            scoreViewModel.historicalMissCountRecord?.misscount
-                                    ?.toString() ??
-                                '---',
-                            style: const TextStyle(fontSize: 36),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          const Text('Current misscount'),
-                          Text(
-                            scoreViewModel.currentRecord?.misscount
-                                    ?.toString() ??
-                                '---',
-                            style: const TextStyle(fontSize: 36),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+    final barGroups = scoreViewModel.scores.reversed
+        .groupListsBy((e) => e.versionId)
+        .entries
+        .map(
+          (e) => BarChartGroupData(
+            x: e.key,
+            barRods: [
+              BarChartRodData(
+                y: e.value.last.score.toDouble(),
+                width: 25,
+                borderRadius: const BorderRadius.all(Radius.zero),
+                colors: e.value.last.versionId == 29
+                    ? [Colors.blue]
+                    : [Colors.green],
               ),
-            ),
+            ],
           ),
-          Card(
-            margin: const EdgeInsets.all(10),
-            child: Container(
-              padding: const EdgeInsets.all(5),
-              width: double.infinity,
-              child: Column(
-                children: [
-                  const Text(
-                    'Score Update Detail',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  FittedBox(
-                    child: DataTable(
-                      columnSpacing: 20,
-                      columns: const [
-                        DataColumn(
-                          label: Text('Version'),
-                        ),
-                        DataColumn(
-                          label: Text('Update\r\nDate'),
-                        ),
-                        DataColumn(
-                          label: Text('Clear\r\nLamp'),
-                        ),
-                        DataColumn(
-                          label: Text('Best\r\nScore'),
-                        ),
-                        DataColumn(
-                          label: Text('Score\r\nPace'),
-                        ),
-                        DataColumn(
-                          label: Text('Miss\r\nCount'),
-                        ),
-                      ],
-                      rows: scoreViewModel.scores
-                          .map(
-                            (e) => DataRow(
-                              cells: [
-                                DataCell(Text(e.version
-                                    .toString()
-                                    .replaceAll(' ', '\r\n'))),
-                                DataCell(Text(e.updatedAt)),
-                                DataCell(Text(e.clearType
-                                    .toString()
-                                    .replaceAll(' CLEAR', '\r\nCLEAR'))),
-                                DataCell(Container(
-                                  child: Text(e.score.toString()),
-                                  alignment: Alignment.centerRight,
-                                )),
-                                DataCell(Container(
-                                  child: Text(
-                                      '${e.scorePace}\r\n${e.nextScorePace}'),
-                                  alignment: Alignment.center,
-                                )),
-                                DataCell(
-                                  Container(
-                                    child:
-                                        Text(e.misscount?.toString() ?? '---'),
-                                    alignment: Alignment.centerRight,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  ),
-                ],
-              ),
+        )
+        .toList();
+    final versionDictionary = scoreViewModel.scores
+        .groupListsBy((e) => e.versionId)
+        .map((key, value) =>
+            MapEntry(key, value.first.version.replaceAll(' ', '\r\n')));
+    final scoreBorder = {
+      scoreViewModel.chartDetail?.rankA: 'A',
+      scoreViewModel.chartDetail?.rankAAMinus: 'AA-',
+      scoreViewModel.chartDetail?.rankAA: 'AA',
+      scoreViewModel.chartDetail?.rankAAAMinus: 'AAA-',
+      scoreViewModel.chartDetail?.rankAAA: 'AAA',
+      scoreViewModel.chartDetail?.maxMinus: 'Max-',
+      scoreViewModel.chartDetail?.max: 'Max',
+    };
+    return Container(
+      padding: const EdgeInsets.fromLTRB(10, 50, 10, 50),
+      child: BarChart(
+        BarChartData(
+          titlesData: FlTitlesData(
+            show: true,
+            leftTitles: SideTitles(
+              showTitles: true,
+              interval: 1,
+              reservedSize: 40,
+              getTitles: (v) =>
+                  '${scoreBorder[v.toInt()]}\r\n${v.toInt().toString()}',
+              checkToShowTitle:
+                  (minValue, maxValue, sideTitles, appliedInterval, value) =>
+                      scoreBorder.containsKey(value.toInt()),
             ),
-          )
-        ],
+            topTitles: SideTitles(showTitles: false),
+            rightTitles: SideTitles(showTitles: false),
+            bottomTitles: SideTitles(
+                showTitles: true,
+                margin: 10,
+                rotateAngle: 45,
+                getTitles: (v) {
+                  return versionDictionary[v.toInt()] ?? '';
+                }),
+          ),
+          barGroups: barGroups,
+          maxY: scoreViewModel.chartDetail?.max.toDouble(),
+          minY: scoreViewModel.chartDetail?.rankA.toDouble(),
+          gridData: FlGridData(
+              show: true,
+              horizontalInterval: 1,
+              checkToShowHorizontalLine: (v) {
+                return scoreBorder.containsKey(v.toInt());
+              }),
+          rangeAnnotations: RangeAnnotations(
+            horizontalRangeAnnotations: [
+              HorizontalRangeAnnotation(
+                  y1: scoreViewModel.chartDetail?.rankA.toDouble() ?? 0,
+                  y2: scoreViewModel.chartDetail?.rankAAMinus.toDouble() ?? 0,
+                  color: Colors.green.shade100),
+              HorizontalRangeAnnotation(
+                  y1: scoreViewModel.chartDetail?.rankAAMinus.toDouble() ?? 0,
+                  y2: scoreViewModel.chartDetail?.rankAA.toDouble() ?? 0,
+                  color: Colors.green.shade200),
+              HorizontalRangeAnnotation(
+                  y1: scoreViewModel.chartDetail?.rankAA.toDouble() ?? 0,
+                  y2: scoreViewModel.chartDetail?.rankAAAMinus.toDouble() ?? 0,
+                  color: Colors.grey.shade200),
+              HorizontalRangeAnnotation(
+                  y1: scoreViewModel.chartDetail?.rankAAAMinus.toDouble() ?? 0,
+                  y2: scoreViewModel.chartDetail?.rankAAA.toDouble() ?? 0,
+                  color: Colors.grey.shade300),
+              HorizontalRangeAnnotation(
+                  y1: scoreViewModel.chartDetail?.rankAAA.toDouble() ?? 0,
+                  y2: scoreViewModel.chartDetail?.maxMinus.toDouble() ?? 0,
+                  color: Colors.yellow.shade100),
+              HorizontalRangeAnnotation(
+                y1: scoreViewModel.chartDetail?.maxMinus.toDouble() ?? 0,
+                y2: scoreViewModel.chartDetail?.max.toDouble() ?? 0,
+                color: Colors.yellow.shade200,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
