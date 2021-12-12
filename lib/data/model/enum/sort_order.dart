90785551cdc85ca6dc13beca56e9c309b1c6da4e
kmkmtrtr <arctic-wind@hotmail.co.jp>
2021-12-12 19:36:02 +0900

    fix order bug

--- lib/data/model/enum/sort_order.dart
+++ lib/data/model/enum/sort_order.dart
@@ -11,15 +11,4 @@ extension SortOrderExt on SortOrder {
         return '';
     }
   }
-
-  int get index {
-    switch (this) {
-      case SortOrder.asc:
-        return 0;
-      case SortOrder.desc:
-        return 1;
-      default:
-        return 0;
-    }
-  }
 }
